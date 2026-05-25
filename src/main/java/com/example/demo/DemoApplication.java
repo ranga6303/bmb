7b19d93659781821f22e4d421cbd7614caf1dbb5
ranga6303 <jangamrangaswaamy75@gmail.com>
2026-05-25 16:32:04 +0530

    feat: add logging to email service for debugging

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
+@EnableAsync
 @EnableScheduling
 public class DemoApplication {
 
--- src/main/java/com/example/demo/service/SmtpEmailService.java
+++ src/main/java/com/example/demo/service/SmtpEmailService.java
@@ -1,35 +1,55 @@
 package com.example.demo.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SmtpEmailService implements EmailService {
+    private static final Logger logger = LoggerFactory.getLogger(SmtpEmailService.class);
+
     private final JavaMailSender mailSender;
 
     @Value("${app.mail.from}")
     private String fromAddress;
+
+    @Value("${spring.mail.host}")
+    private String mailHost;
+
+    @Value("${spring.mail.username}")
+    private String mailUsername;
 
     public SmtpEmailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
     @Override
+    @Async
     public void sendPasswordResetOtp(String to, String otp) {
         send(to, "Your password reset OTP", "Your password reset OTP is: " + otp + ". It expires in 15 minutes.");
     }
 
     @Override
+    @Async
     public void sendOtpEmail(String to, String otp) {
         send(to, "Your registration OTP", "Your OTP is: " + otp + ". It expires in 10 minutes.");
     }
 
     private void send(String to, String subject, String body) {
+        logger.info("Sending email using host={}, username={}, fromAddress={}", mailHost, mailUsername, fromAddress);
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(fromAddress);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
-        mailSender.send(message);
+        try {
+            mailSender.send(message);
+        } catch (Exception e) {
+            logger.error("Failed to send email to {}", to, e);
+        }
     }
 }
